24d5d8b7b40eece7e2a92995854877bd237ba9c9
LonelyGode <cg18838031307@gmail.com>
2019-04-15 21:26:23 +0800

    add D2-3

--- Pintia/Data Structure Exercise 2/7-3-SpecialQueue.cpp
+++ Pintia/Data Structure Exercise 2/7-3-SpecialQueue.cpp
@@ -20,73 +20,77 @@ void InitQueue(SqQueue &Q, int  l) {
     Q.front = Q.rear = 0;
 }
 
-void EnQueue (SqQueue &Q, char e) {
-    if((Q.rear + 1) % l == Q.front) {
+void EnQueue (SqQueue &Q, int  e) {
+    if(Q.rear == l) {
         cout << "Full Queue" << endl;
         return;
     }
-    Q.base[Q.rear ] = e;
-    Q.rear = (Q.rear + 1) % l;
+    Q.base[Q.rear] = e;
+    Q.rear++;
 }
 
 void DeQueue(SqQueue &Q) {
     if(Q.front == Q.rear) {
         cout << "Empty Queue" << endl;
-        return;
+    } else {
+        cout << Q.base[0] << endl;
+        for(int i = 0; i < Q.rear; i++) {
+            Q.base[i] = Q.base[i + 1];
+        }
+        Q.rear--;
     }
-    Q.front = (Q.front + 1 ) % l ;
+}
+
+void DeleteMid(SqQueue &Q) {
+    int mid;
+    if(Q.rear % 2) {
+        mid = (Q.rear + 1) / 2;
+    } else {
+        mid = Q.rear / 2;
+    }
+    mid--;
+    cout << Q.base[mid] << endl;
+    for(int i = mid; i < Q.rear; i++) {
+        Q.base[i] = Q.base[i + 1];
+    }
+    Q.rear--;
+}
+
+void PrintQueue(SqQueue Q) {
+    bool flag = true;
+    for(int i = 0; i < Q.rear ; i++) {
+        if(flag) {
+            cout << Q.base[i];
+            flag = false;
+        } else {
+            cout << " " << Q.base[i];
+        }
+    }
 }
 
 int main() {
     int m;
     cin >> m >> l;
-    cout << "L: " <<  l << endl;
-    SqQueue Q;
-    InitQueue(Q, l);
-    string command;
-    int number;
-    for(int i = 0; i < m; i++) {
-        cin >> command;
-        // number = 0;
-        if(command == "DeQueue") {
-            DeQueue(Q);
+    if(m < 1000000 && l < 1000000) {
+
+        SqQueue Q;
+        InitQueue(Q, l);
+        string command;
+        int number;
+        for(int i = 0; i < m; i++) {
+            cin >> command;
+            if(command == "DeQueue") {
+                DeQueue(Q);
+            }
+            if(command == "EnQueue") {
+                cin >> number;
+                EnQueue(Q, number);
+            }
+            if(command == "DeleteMid") {
+                DeleteMid(Q);
+            }
         }
-        if(command == "EnQueue") {
-            cin >> number;
-            EnQueue(Q, number);
-        }
+        PrintQueue(Q);
     }
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
